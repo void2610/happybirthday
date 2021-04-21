21d24829aa1bbe1029b3932697f8e021d7152600
void2610 <52399972+void2610@users.noreply.github.com>
2021-04-21 18:53:19 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ alert("貴方のiPhoneはおめでとうウイルスに感染しました");
 alert("ウイルスを除去するには並河からプレゼントを受け取る必要があります");
 
 
-let img = document.getElementsByTagName("img");;
+let img = document.getElementById("img");
 let angle = 0;
 setInterval(aaa,10);
 function aaa (){
-    for(let i = 0; i < 27; i++){
-        img[i].style.transform = "rotate(" + angle + "deg)";
-    }
+        img.style.transform = "rotate(" + angle + "deg)";
+    
     angle++;
 }
